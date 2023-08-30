ef5515f19a9a4a5b1df58e2305fb45d7ac8723b9
Sai Nair <74823075+sainair@users.noreply.github.com>
2023-08-30 20:25:32 +0300

    Supplier functions added
    Added a function to show supplier details, and one to remove supplier records

--- supplier.py
+++ supplier.py
@@ -1,3 +1,26 @@
 #add supplier management shit here,
 #shit like 
 #supplier contact, add/remove supplier and shit
+
+import mysql.connector as mc
+from mysql.connector import Error
+from dotenv import load_dotenv
+import os
+
+
+def supplierData(cursor):
+    try:
+        x=input("Enter supplier name: ")
+        cursor.execute("SELECT PRODUCT_ID, PRODUCT_NAME, COST_PRICE, SELLING_PRICE, INVENTORY from products where SUPPLIER=%s", (x,))
+        for val in cursor:
+            print(val)
+    except Error as err:
+        print(err)
+
+def removeSupplier(cursor):
+    try:
+        supplier=input("Enter supplier name: ")
+        cursor.execute("DELETE from products WHERE SUPPLIER=%s", (supplier,))
+        print(f"Supplier {supplier} removed")
+    except Error as err:
+        print(err)
