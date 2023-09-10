1ef9cc42674955539e78ab2020d07b6c2aa30e68
Sai Nair <74823075+sainair@users.noreply.github.com>
2023-09-10 20:50:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from dotenv import load_dotenv
 import os
 import Functions.supplier as supplier
 import Functions.inventory as inventory
+import Functions.transaction as transaction
 load_dotenv()
 
 passw = input("Please Enter the Password to MYSQL:- ")
@@ -38,6 +39,9 @@ cur.execute("""CREATE TABLE IF NOT EXISTS `Users` (
 	`Role` VARCHAR(255),
     `Name` VARCHAR(255),
 	PRIMARY KEY (`UserID`));""")
+
+transaction1= transaction.transactions(cur, passw)
+
 def ran_num():
     valid_digits = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     random_number = ''.join([str(random.choice(valid_digits)) for i in range(6)])
@@ -91,6 +95,7 @@ Welcome to the Inventory Management Software
     Reply with 5 to delete a product
     Reply with 6 to view Supplier data
     Reply with 7 to remove Supplier
+    Reply with 8 to switch to Transactions
     Reply with Q to Quit
     """)
 
@@ -99,6 +104,7 @@ Welcome to the Inventory Management Software
 
             if x == "1":
                 inventory.view_data(cur,test_connection)
+                inventory.checkStock(cur)
             elif x=="2":
                 inventory.add_product(cur,test_connection)
                 
@@ -112,6 +118,38 @@ Welcome to the Inventory Management Software
                 supplier.supplierData(cur)
             elif x=="7":
                 supplier.removeSupplier(cur,test_connection)
+            elif x=="8":
+                transaction_cursor = transaction1.cursor()
+                while True:
+                    print("""
+Welcome to the Inventory Management Software
+    Reply with 1 to view transactions 
+    Reply with 2 to add a transaction
+    Reply with 3 to modify a transaction
+    Reply with 4 to fetch details about a transaction
+    Reply with 5 to delete a transaction
+    Reply with 6 to view transaction data
+    Reply with 7 to remove transaction
+    Reply with Q to exit Transactions
+    """)
+                    act=input("-->")
+                    if act==1:
+                        inventory.view_data(transaction_cursor)
+                    elif act=="q".upper():
+                        print("""
+Welcome to the Inventory Management Software
+    Reply with 1 to get data on all prodcuts 
+    Reply with 2 to add a product
+    Reply with 3 to modify a product
+    Reply with 4 to fetch details about a product
+    Reply with 5 to delete a product
+    Reply with 6 to view Supplier data
+    Reply with 7 to remove Supplier
+    Reply with 8 to switch to Transactions
+    Reply with Q to Quit
+    """)
+                        break
+
             elif x.upper()=="Q":
                 break
             else:
