f39f70be4363c6abecd18fb3effeedc5f161e1cc
Sai Nair <74823075+sainair@users.noreply.github.com>
2023-09-11 06:08:17 +0300

    Update inventory.py (#7)
    * Update inventory.py
    
    * Update transaction.py
    
    * Update main.py

--- Functions/transaction.py
+++ Functions/transaction.py
@@ -1,4 +1,15 @@
 #make  a table transactions
+import mysql.connector as mc
+from mysql.connector import Error
+
+def transactions(cursor, password):
+    try:
+        connection = mc.connect(host = "localhost", database="test1", user="root", passwd=password)
+        cursor.execute("CREATE TABLE IF NOT EXISTS Transactions(Day int, Quantity int, Sales int, Profit int, Loss int)")
+        return connection
+    except Error as err:
+        print(err)
+
 #daily u record daily(user input) total sales, ie SUNDAY, MONDAY, TUESDAY etc
 #using this calculate that day's profit and loss
 #remove sale volume from inventory
